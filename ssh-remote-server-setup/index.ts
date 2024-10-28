bdbea123555adef0d299cab02e79f4f0d8a0a0ca
Kenny Ho <kennyho.code@gmail.com>
2024-10-28 15:02:25 -0400

    ssh remote server setup

--- ssh-remote-server-setup/index.ts
+++ ssh-remote-server-setup/index.ts
@@ -1,26 +1,19 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as digitalocean from "@pulumi/digitalocean";
-import * as fs from "fs";
 import * as path from "path";
 import * as os from "os";
+import * as fs from "fs";
 
-// Function to read public key
 function readPublicKey(): string {
-  try {
-    // Read from default .ssh directory in home directory
-    const keyPath = path.join(os.homedir(), ".ssh/pulumi", "id_rsa.pub");
-    return fs.readFileSync(keyPath, "utf8").trim();
-  } catch (error) {
-    console.error("Error reading public key:", error);
-    throw error;
-  }
+  const sshKeyPath = path.resolve(os.homedir(), ".ssh/pulumi", "id_rsa.pub");
+  return fs.readFileSync(sshKeyPath, "utf-8").trim();
 }
 
 const sshKey = new digitalocean.SshKey("my-ssh-key", {
   publicKey: readPublicKey(),
 });
 
-const droplet = new digitalocean.Droplet("my-droplet", {
+const web = new digitalocean.Droplet("web", {
   image: "ubuntu-20-04-x64",
   name: "web-1",
   region: digitalocean.Region.NYC2,
@@ -28,4 +21,4 @@ const droplet = new digitalocean.Droplet("my-droplet", {
   sshKeys: [sshKey.fingerprint],
 });
 
-export const dropletIp = droplet.ipv4Address;
+export const ipAddress = web.ipv4Address;
